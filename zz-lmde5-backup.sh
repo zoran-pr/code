2c7db3d5d386aca8f030656b9c1489d5049f43f0
Zoran Pr <no-reply@vpnetcanada.com>
2022-09-24 20:56:53 -0400

    updated zz-lmde5-backup.sh

--- zz-lmde5-backup.sh
+++ zz-lmde5-backup.sh
@@ -9,13 +9,20 @@ backup_dir="${backup_path}/${HOSTNAME%%.*}_$(date +%Y-%m-%d)"
 
 # create backup dir
 mkdir ${backup_dir}
+sudo chown root:users ${backup_dir}
+sudo chmod 775 ${backup_dir}
 
 # backup all users
 sudo rsync -av /home ${backup_dir}/
+sudo rsync -av /opt ${backup_dir}/
+sudo rsync -av /etc ${backup_dir}/
+sudo rsync -av /boot ${backup_dir}/
+# vmware, database, we servrers etc.
+#sudo rsync -av /var ${backup_dir}/
 
 # backup all installed deb's
-sudo apt-key add ${backup_dir}/apt-trusted-keys
-local$ dpkg --get-selections > ${backup_dir}/apt-packages.list
+#sudo apt-key exportall > ${backup_dir}/apt-trusted-keys
+dpkg --get-selections > ${backup_dir}/apt-packages.list
 
 # backup code settings and extensions
 code --list-extensions | xargs -L 1 echo code --install-extension > ${backup_dir}/ext
